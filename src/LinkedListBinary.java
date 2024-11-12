5c1f36a005393f44023b2d53bfa0da0efbc95600
Ainur Kaishibayeva <akaishibayeva@gmail.com>
2024-11-11 18:46:53 -0600

    Convert binary into decimal

---
+++ src/LinkedListBinary.java
@@ -0,0 +1,111 @@
+public class LinkedListBinary {
+
+    private Node head;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedListBinary(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        if (temp == null) {
+            System.out.println("empty");
+        } else {
+            while (temp != null) {
+                System.out.print(temp.value);
+                temp = temp.next;
+                if (temp != null) {
+                    System.out.print(" -> ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        length++;
+    }
+
+    //   +===================================================+
+    //   |               WRITE YOUR CODE HERE                |
+    //   | Description:                                      |
+    //   | - This method converts a binary number,           |
+    //   |   represented as a linked list, to a decimal int. |
+    //   |                                                   |
+    //   | Return type: int                                  |
+    //   | - Returns the decimal equivalent of the binary    |
+    //   |   number.                                         |
+    //   |                                                   |
+    //   | Tips:                                             |
+    //   | - We use a while loop to traverse the linked list.|
+    //   | - Multiply the current sum by 2 and add the value |
+    //   |   at each node to get the decimal number.         |
+    //   +===================================================+
+
+    public int binaryToDecimal() {
+        int num = 0;
+
+        Node current = head;
+
+        while(current != null) {
+                num = (num * 2) + current.value;
+                current = current.next;
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        LinkedListBinary list = new LinkedListBinary(1);
+        list.append(0);
+        list.append(1);
+//        list.append(1);  // Duplicate
+//        list.append(4);
+//        list.append(2);  // Duplicate
+//        list.append(5);
+
+        System.out.println("Original list:");
+        list.printList();
+
+        int decimalValue = list.binaryToDecimal();
+        System.out.println("The decimal value of the binary linked list is: " + decimalValue);
+
+    }
+
+}
+
+
